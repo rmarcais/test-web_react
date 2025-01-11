b970c0a019f7a0af847619b96f2ee0b6696c8518
Rémi Marçais <remi.marcais87@gmail.com>
2025-01-11 21:21:45 +0100

    Use type assertion in sumMinorCredits and sumMajorCredits functions

--- TypeScript/task_5/js/main.ts
+++ TypeScript/task_5/js/main.ts
@@ -9,9 +9,9 @@ interface MinorCredits {
 }
 
 function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-    return {credits: subject1.credits + subject2.credits, __brand: "Major"};
+    return {credits: subject1.credits + subject2.credits} as MajorCredits;
 }
 
 function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-    return {credits: subject1.credits + subject2.credits, __brand: "Minor"};
+    return {credits: subject1.credits + subject2.credits} as MinorCredits;
 }
